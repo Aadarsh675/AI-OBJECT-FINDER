a720e01e65c7b656eb83448d7205728d0c6f9dc7
Aadarsh675 <76542875+Aadarsh675@users.noreply.github.com>
2021-10-30 09:45:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function setup() {
     canvas = createCanvas(380, 380);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(380, 380);
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Object";
 }
